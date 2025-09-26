4745c441a86b38b8c8394f1fe4cc7dbfae253e25
Eurus <ilianjesus14@outlook.com>
2025-09-26 14:02:21 -0600

    ruta base para ghpages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Formulario from "./pages/Formulario";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/formulario" element={<Formulario />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
-
-export default App;
